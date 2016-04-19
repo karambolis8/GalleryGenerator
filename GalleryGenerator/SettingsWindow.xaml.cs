3e60853438c3c5499333f357309163ce377be4ee
Adam Gudynowski <karambolis8@gmail.com>
2016-04-19 10:04:08 +0200

    implemented settings window

--- GalleryGenerator/SettingsWindow.xaml.cs
+++ GalleryGenerator/SettingsWindow.xaml.cs
@@ -12,16 +12,27 @@ namespace GalleryGenerator
 
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
-            //var defWidth = Settings.Default.Properties["AppWidth"].DefaultValue;
+            this.MediumImgWidthTextBox.Text = (string) Settings.Default.Properties["MediumImgWidth"].DefaultValue;
+            this.MediumImgHeightTextBox.Text = (string) Settings.Default.Properties["MediumImgHeight"].DefaultValue;
+            this.ThumbImgWidthTextBox.Text = (string) Settings.Default.Properties["ThumbImgWidth"].DefaultValue;
+            this.ThumbImgHeightTextBox.Text = (string) Settings.Default.Properties["ThumbImgHeight"].DefaultValue;
+
+            string boolStr = (string)Settings.Default.Properties["PreserveMediumAspectRatio"].DefaultValue;
+            this.PreserveAspectRatioCheckBox.IsChecked = boolStr == "True";
+            boolStr = (string)Settings.Default.Properties["CopyOriginalFiles"].DefaultValue;
+            this.CopyOriginalFilesCheckBox.IsChecked = boolStr == "True";
+
         }
 
         private void SaveButton_OnClick(object sender, RoutedEventArgs e)
         {
+            Settings.Default.Save();
             this.Close();
         }
 
         private void CancelButton_OnClick(object sender, RoutedEventArgs e)
         {
+            Settings.Default.Reload();
             this.Close();
         }
     }
